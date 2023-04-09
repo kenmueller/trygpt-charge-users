eef3596b79a7170d65e3dd4f5e3de28376314381
Ken Mueller <kenmueller0@gmail.com>
2023-04-08 17:00:26 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+if (!process.env.ORIGIN) throw new Error('Missing ORIGIN')
+if (!process.env.TOKEN) throw new Error('Missing TOKEN')
+
+class HttpError extends Error {
+	constructor(code, message) {
+		super(message)
+		this.code = code
+	}
+}
+
+const main = async () => {
+	console.log('Charging users...')
+
+	const response = await fetch(
+		`${process.env.ORIGIN}/api/stripe/charge-users`,
+		{
+			method: 'POST',
+			headers: {
+				authorization: `Bearer ${process.env.TOKEN}`
+			}
+		}
+	)
+
+	if (!response.ok)
+		throw new HttpError(response.status, await response.text())
+
+	console.log('Chargin users... DONE')
+}
+
+main().catch(console.error)
